6520cef1ef77dd3d22bc56efdd4aaa3d9034ae6d
nziza jules <nzizajules360@gmail.com>
2025-11-11 08:49:15 +0000

    fix this error  and make the notifcation sending work in the developer d

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,19 +1,14 @@
+
 import * as admin from 'firebase-admin';
 import { getApps } from 'firebase-admin/app';
 
 export function initAdmin() {
   if (getApps().length === 0) {
-    const serviceAccount = {
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    };
-
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
     });
   }
-
   return {
     auth: admin.auth(),
     firestore: admin.firestore(),
